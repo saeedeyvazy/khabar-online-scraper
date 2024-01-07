bb46f50e626152940a75a448477757d0a6f07f63
saeed <saeed.eyvazy@gmail.com>
2024-01-07 07:13:15 +0330

    Change main file name

--- khabaronline/khbar-online-main.py
+++ khabaronline/khbar-online-main.py
@@ -5,7 +5,7 @@ import os
 
   
 print('Start Scheduler' )
-schedule.every(2).minutes.do(lambda: os.system('scrapy crawl khabaronline -s JOBDIR=crawls/khabaronline')) 
+schedule.every(30).seconds.do(lambda: os.system('scrapy crawl khabaronline -s JOBDIR=crawls/khabaronline')) 
 print('Next job is set to run at: ' + str(schedule.next_run()))
   
 # infinite loop to run the scheduled spider 
